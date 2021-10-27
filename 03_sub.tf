30481caa1e4af9711afc9e7c491e82570f724748
majeong <alsdk5957@gmail.com>
2021-10-27 15:52:05 +0900

    subnet_step3

---
+++ 03_sub.tf
@@ -0,0 +1,43 @@
+#가용영역 a의 Public Subnet
+resource "aws_subnet" "minajeong_puba" {
+  vpc_id            = aws_vpc.minajeong_vpc.id
+  cidr_block        = "10.0.0.0/24"
+  availability_zone = "ap-northeast-2a"
+
+  tags = {
+    Name = "minajeong-puba"
+  }
+}
+
+#가용영역 a의 Private Subnet
+resource "aws_subnet" "minajeong_pria" {
+  vpc_id            = aws_vpc.minajeong_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ap-northeast-2a"
+
+  tags = {
+    Name = "minajeong-pria"
+  }
+}
+
+#가용영역 c의 Public Subnet
+resource "aws_subnet" "minajeong_pubc" {
+  vpc_id            = aws_vpc.minajeong_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "ap-northeast-2c"
+
+  tags = {
+    Name = "minajeong-pubc"
+  }
+}
+
+#가용영역 c의 Private Subnet
+resource "aws_subnet" "minajeong_pric" {
+  vpc_id            = aws_vpc.minajeong_vpc.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "ap-northeast-2c"
+
+  tags = {
+    Name = "minajeong-pric"
+  }
+}
