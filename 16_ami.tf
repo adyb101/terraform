c835d1910bda21b20b66f073b12d31b5a4c5e1a8
majeong <alsdk5957@gmail.com>
2021-11-01 10:27:57 +0900

    ami_step16

--- 16_ami.tf
+++ 16_ami.tf
@@ -1,4 +1,7 @@
 resource "aws_ami_from_instance" "minajeong_ami" {
   name               = "minajeong-ami"
   source_instance_id = aws_instance.minajeong_weba.id
+  depends_on = [
+    aws_instance.minajeong_weba
+  ]
 }
