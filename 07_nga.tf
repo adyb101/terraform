75166b8e7a18715780374231fa431be26c73611f
majeong <alsdk5957@gmail.com>
2021-11-01 10:23:11 +0900

    natgateway_step7

--- 07_nga.tf
+++ 07_nga.tf
@@ -10,7 +10,7 @@ resource "aws_eip" "lb_ip_c" {
 
 resource "aws_nat_gateway" "minajeong_nga_a" {
   allocation_id = aws_eip.lb_ip_a.id
-  subnet_id     = aws_subnet.minajeong_pria.id
+  subnet_id     = aws_subnet.minajeong_puba.id
 
   tags = {
     "Name" = "minajeong-nga-a"
@@ -19,7 +19,7 @@ resource "aws_nat_gateway" "minajeong_nga_a" {
 
   resource "aws_nat_gateway" "minajeong_nga_c" {
   allocation_id = aws_eip.lb_ip_c.id
-  subnet_id     = aws_subnet.minajeong_pric.id
+  subnet_id     = aws_subnet.minajeong_pubc.id
 
   tags = {
     "Name" = "minajeong-nga-c"
