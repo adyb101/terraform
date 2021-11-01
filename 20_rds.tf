c51a8b37823cf377bd459c7ae916e5f425eedfb6
majeong <alsdk5957@gmail.com>
2021-11-01 10:35:19 +0900

    rds_step20

---
+++ 20_rds.tf
@@ -0,0 +1,29 @@
+resource "aws_db_instance" "minajeong_mydb" {
+  allocated_storage        = 20
+  storage_type             = "gp2"
+  engine                   = "mysql"
+  engine_version           = "8.0"
+  instance_class           = "db.t2.micro"
+  name                     = "mydb"
+  identifier               = "mydb"
+  username                 = "admin"
+  password                 = "It12345!"
+  parameter_group_name     = "default.mysql8.0"
+  availability_zone        = "ap-northeast-2a"
+  db_subnet_group_name     = aws_db_subnet_group.minajeong_dbsn.id
+  vpc_security_group_ids   = [aws_security_group.minajeong_websg.id]
+  skip_final_snapshot      = true
+  
+  tags  =   {
+      "Name"    =   "minajeong-mydb"
+    }
+  }
+
+  resource "aws_db_subnet_group" "minajeong_dbsn" {
+      name = "minajeong-dbsb-group"
+      subnet_ids = [aws_subnet.minajeong_pridba.id,aws_subnet.minajeong_pridbc.id]
+
+      tags = {
+        "Name" = "minajeong-dbsb-group"
+      }
+  }
