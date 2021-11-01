7b4dc9d6be1a973fcd370799c1724ee709b7b875
majeong <alsdk5957@gmail.com>
2021-11-01 10:26:25 +0900

    instance_step11

--- 11_web.tf
+++ 11_web.tf
@@ -15,17 +15,23 @@ data "aws_ami" "amzn" {
 }
 
 resource "aws_instance" "minajeong_weba" {
-    ami                         = data.aws_ami.amzn.id
+    ami                         = "ami-0e4a9ad2eb120e054"
     instance_type               = "t2.micro"
     key_name                    = "tf-key1"
-    vpc_security_group_ids      = [aws_security_group.minajeogn_websg.id]
+    vpc_security_group_ids      = [aws_security_group.minajeong_websg.id]
     availability_zone           = "ap-northeast-2a"
     private_ip                  = "10.0.0.11"
-    associate_public_ip_address = true
     subnet_id                   = aws_subnet.minajeong_puba.id
     user_data                   = file("./install_seoul.sh")
 
     tags   =   {
         Name = "minajeong-weba"
     }
+}
+
+resource "aws_eip" "minajeong_web_eip" {
+  vpc = true
+  instance                    = aws_instance.minajeong_weba.id
+  associate_with_private_ip   = "10.0.0.11"
+  depends_on                  = [aws_internet_gateway.minajeong_ig]
 }
